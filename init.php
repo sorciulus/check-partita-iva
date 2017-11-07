21bf02670447c3cc11264790ba077e09ac9b84c1
ronci-c <corrado.r@docsity.com>
2017-11-07 11:01:01 +0100

    add trevis file + fix

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-use sorciulus\PartitaIva\PartitaIva;
-
-$class = new PartitaIva();
-$isValid = $class->setPartitaIva(null)->isValid();
-var_dump($isValid);
